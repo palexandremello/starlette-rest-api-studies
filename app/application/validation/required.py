0ac8f001ada9e7a14f96682e0649c402f83ee27b
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-09-03 15:54:03 -0300

    feat: create RequiredString Validator

--- app/application/validation/required.py
+++ app/application/validation/required.py
@@ -9,6 +9,15 @@ class Required(Validator):
         self.field_name = field_name
 
     def validate(self) -> Exception or None:
-        if (self.value == None or self.field_name == None):
+        if (self.value == None or self.field_name == ''):
             return RequiredFieldError(self.field_name)
 
+class RequiredString(Required):
+    def __init__(self, value: any, field_name: str) -> None:
+        super().__init__(value, field_name)
+
+    def validate(self) -> Exception or None:
+        print(super().validate() != None)
+        print(self.value == '')
+        if (super().validate() != None or self.value == ''):
+            return RequiredFieldError(self.field_name)
--- tests/application/validaton/test_required_string.py
+++ tests/application/validaton/test_required_string.py
@@ -1,18 +1,22 @@
 
 from app.application.errors.validation import RequiredFieldError
-from app.application.validation.required import Required
-from app.application.validation.validator import Validator
+from app.application.validation.required import Required, RequiredString
 
 
 def make_sut():
-    return Required(None, 'any_field')
+    return RequiredString('', 'any_field')
 
 
 def test_should_be_same_instance():
     sut = make_sut()
-    assert isinstance(sut, Validator)
+    assert isinstance(sut, Required)
 
-def test_should_return_RequiredFieldError_if_value_is_none():
-    sut = Required(None, 'any_field')
+def test_should_return_RequiredFieldError_if_value_is_empty():
+    sut = RequiredString('', 'any_field')
     error = sut.validate()
-    assert isinstance(error, RequiredFieldError)
+    assert isinstance(error, RequiredFieldError), f"Expected: {RequiredFieldError}, Result: {error}"
+
+def test_should_return_None_if_value_is_not_empty():
+    sut = RequiredString('any_value', 'any_field')
+    error = sut.validate()
+    assert error == None, f"Expected: {None}, Result: {error}"
