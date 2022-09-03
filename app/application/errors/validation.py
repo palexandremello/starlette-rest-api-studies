191bd599cc1139f1635567dabf4a962b23c8551c
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-09-03 20:30:35 -0300

    feat: MaxFileSize Validator

--- app/application/errors/validation.py
+++ app/application/errors/validation.py
@@ -3,3 +3,9 @@ class RequiredFieldError(Exception):
         self.message = 'Field required' if field_name == None else f'The field {field_name} is required'
         print(self.message)
         super().__init__(self.message)
+
+
+class MaxFileSizeError(Exception):
+    def __init__(self, max_size_in_mb) -> None:
+        self.message = f'File upload limit is {max_size_in_mb} MB'
+        super().__init__(self.message)
