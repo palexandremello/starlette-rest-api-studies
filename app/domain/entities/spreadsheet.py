32fc069d23a715947c1b6d5bb5b3f81f45ee0bca
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-09-07 04:14:39 -0300

    feat: new methods on Spreadsheet entity

--- app/domain/entities/spreadsheet.py
+++ app/domain/entities/spreadsheet.py
@@ -1,10 +1,18 @@
+import dataclasses
 from datetime import datetime
-from typing import NamedTuple
 
-
-class Spreadsheet(NamedTuple):
-    id: str
+@dataclasses.dataclass
+class Spreadsheet:
+    id: int
     filename: str
     initial_date: datetime
     final_date: datetime
     link: str
+
+
+    @classmethod
+    def from_dict(cls, dictonary):
+        return cls(**dictonary)
+    
+    def to_dict(self):
+        return dataclasses.asdict(self)
--- tests/domain/entities/test_spreadsheet.py
+++ tests/domain/entities/test_spreadsheet.py
@@ -1,10 +1,7 @@
 from app.domain.entities.spreadsheet import Spreadsheet
 def test_should_be_able_create_a_Spreadsheet_object():
-    conciliation = {"id":'any_id', "filename":'any_file', "initial_date":'any_date', 'final_date':'any_date', "link":'any_link'}
-    sut = Spreadsheet(**conciliation)
+    spreadsheet_data = {"id":'any_id', "filename":'any_file', "initial_date":'any_date', 'final_date':'any_date', "link":'any_link'}
 
-    assert sut.filename == conciliation['filename']
-    assert sut.initial_date == conciliation['initial_date']
-    assert sut.final_date == conciliation['final_date']
-    assert sut.link == conciliation['link']
-    assert sut.id == conciliation['id']
+    sut = Spreadsheet.from_dict(spreadsheet_data)
+
+    assert sut.to_dict() == spreadsheet_data
