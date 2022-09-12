0f9ca399932cfefe306d6e240645f11b1e182256
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-09-12 15:22:41 -0300

    feat: middlewares cors allow headers

--- app/main/middlewares/index.py
+++ app/main/middlewares/index.py
@@ -2,5 +2,10 @@ from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 
 middlewares = [
-    Middleware(CORSMiddleware, allow_origins=['*'])
+    Middleware(CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['POST', 'GET'],
+    allow_headers=['access-control-allow-origin', 'authorization', 'content-type'],
+
+),
 ]
