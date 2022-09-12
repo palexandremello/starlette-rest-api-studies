fc01857090768d23d086be019f281ace048008fe
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-09-12 15:17:46 -0300

    feat: middlewares cors

--- app/main/config/app.py
+++ app/main/config/app.py
@@ -1,7 +1,8 @@
 from starlette.applications import Starlette
+from app.main.middlewares import middlewares
 from app.main.routes import routes
 
 def startup():
     print("Go on!!")
 
-app = Starlette(debug=True, routes=routes, on_startup=[startup])
+app = Starlette(debug=True, routes=routes, on_startup=[startup], middleware=middlewares)
