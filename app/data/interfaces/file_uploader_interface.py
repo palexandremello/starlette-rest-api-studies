40eb32b932470f8bcb8da9ec5b5635c219098ef6
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-09-14 01:31:53 -0300

    feat: FileUploaderInterface data

---
+++ app/data/interfaces/file_uploader_interface.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from typing import List
+from app.domain.entities.file import File
+
+
+class FileUploader(ABC):
+    """ Interface to FileUploader """
+
+    @abstractmethod
+    async def upload(self, files: List[File] or File) -> List[str] or str:
+        raise Exception("not implemented upload method")
