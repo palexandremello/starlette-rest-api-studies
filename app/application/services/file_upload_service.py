c2eab24984ad3ec9d87ae826d0d3339d5d0659e1
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-09-16 14:37:05 -0300

    feat: FileUploadService method

---
+++ app/application/services/file_upload_service.py
@@ -0,0 +1,18 @@
+from typing import List, Type
+from app.domain.entities.file import File
+from app.domain.usecases.file_upload import FileUpload
+from app.main.interface.service import ServiceInterface
+
+class FileUploadService(ServiceInterface):
+
+    def __init__(self, upload_file_use_case: Type[FileUpload]) -> None:
+        self.upload_file_use_case = upload_file_use_case
+        self.files = []
+
+    async def perfom(self, files: dict) -> List[str] or Exception:
+        for file in files:
+            self.files.append(File(file['filename'], len(file['file'].getvalue()), 'xlsx', 'xlsx', file['file'] ))
+
+        list_of_key =  await self.upload_file_use_case.upload(self.files)
+
+        return list_of_key
